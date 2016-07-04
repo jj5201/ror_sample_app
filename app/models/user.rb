d82dfffbe365c129d89e0d95e6138f5b8419e12d
jj5201 <jj5201@gmail.com>
2016-07-04 21:00:21 +0000

    Finished user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
+    #password must have one special character and number
     
 end
